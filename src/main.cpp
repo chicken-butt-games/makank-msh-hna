a162b9ceab489961bb8698dfa3a5ca67e54028fe
Muhamed Hassan <muhamedhassan8@icloud.com>
2019-06-08 15:39:28 +0200

    Testing the engine's layers

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <NotAVegetable.h>
 
+class ExampleLayer : public NotAVegetable::Layer {
+public:
+    ExampleLayer()
+            : Layer("Example") {
+
+    }
+
+    void OnUpdate() override {
+        NAV_INFO("ExampleLayer::Update");
+    }
+
+    void OnEvent(NotAVegetable::Event &event) override {
+        NAV_TRACE("{0}", event);
+    }
+};
+
 class MakankMshHna : public NotAVegetable::Application {
 public:
     MakankMshHna() {
-
+        PushLayer(new ExampleLayer());
     }
 
     ~MakankMshHna() {
